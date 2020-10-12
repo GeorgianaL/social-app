95c5f397eed26b33244146902f210364aad1a3c7
georgiana.lingurariu-at-336537318274 <georgiana.lingurariu@endava.com>
2020-10-12 14:47:32 +0300

    Created first API - get posts from json file

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log(`Server can't listen on  port ${port}.`, err);
+  }
+  console.log(`server is listening on port ${port}`);
+});
+
+// Use middleware to set the default Content-Type
+app.use((req, res, next) => {
+  res.header("Content-Type", "application/json");
+  next();
+});
+
+app.get("/posts", (req, res) => {
+  const postsFile = JSON.parse(fs.readFileSync("posts.json").toString());
+  res.send(JSON.stringify(postsFile));
+});
